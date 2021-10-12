3c921d368481acbd25060dc09b888975816d7242
sully <sully@DESKTOP-0ADFBT8>
2021-10-12 05:09:04 -0400

    Add project files.

---
+++ BetterTappers/BetterTapper.cs
@@ -0,0 +1,35 @@
+﻿namespace BetterTappers
+{
+    using StardewModdingAPI;
+    using StardewValley;
+    using StardewValley.Objects;
+    using StardewModdingAPI.Events;
+    using StardewObject = StardewValley.Object;
+
+    internal class BetterTappersLogic
+    {
+        public static void CheckTappers(ModEntry mod, ObjectListChangedEventArgs e)
+        {
+            if (!Context.IsMainPlayer)
+            {
+                return;
+            }
+
+            foreach (var Object in e.Added)
+            {
+                if (Object.Value.name is "Tapper")
+                {
+                    ChangeTapperMinutes(mod, Object.Value);
+                }
+            }
+        }
+
+        public static void ChangeTapperMinutes(ModEntry mod, Object tapper)
+        {
+            if (tapper.MinutesUntilReady > 1600)
+            {
+                tapper.MinutesUntilReady = 1600;
+            }
+        }
+    }
+}
---
+++ BetterTappers/ModEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace BetterTappers
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            Helper.Events.World.ObjectListChanged += this.ObjectListChanged;
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void ObjectListChanged(object sender, ObjectListChangedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            BetterTappersLogic.CheckTappers(this, e);
+        }
+
+    }
+}
