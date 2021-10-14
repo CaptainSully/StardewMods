dc91cedc926843d00bdb2ad3ac47dd933c056f68
CaptainSully <sully.kylej@gmail.com>
2021-10-14 19:30:56 -0400

    SWG actually added to repo this time

---
+++ StartWithGreenhouse/StartWithGreenhouseEntry.cs
@@ -0,0 +1,27 @@
+﻿namespace StartWithGreenhouse
+{
+    using StardewModdingAPI;
+    using StardewValley;
+    using StardewModdingAPI.Events;
+
+    public class StartWithGreenhouseEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            this.Helper.Events.GameLoop.SaveLoaded += this.SaveLoaded;
+        }
+
+        private static void SetPantryFlag()
+        {  
+                Game1.player.mailReceived.Add("ccPantry");
+        }
+        
+        private void SaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+            if (!Game1.player.mailReceived.Contains("ccPantry"))
+                SetPantryFlag();
+        }
+    }
+}
