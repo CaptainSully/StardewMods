a97d948bcf32f933644dc8e6bf91ac435ba98448
CaptainSully <sully.kylej@gmail.com>
2022-07-02 10:35:50 -0400

    Infinite watering cans 1.0

---
+++ InfiniteWateringCan/ModConfig.cs
@@ -0,0 +1,35 @@
+﻿
+namespace InfiniteWateringCan
+{
+    // <summary>The raw mod configuration.</summary>
+    internal class ModConfig
+    {
+       /*********
+       ** Accessors
+       *********/
+        /// <summary>Whether the mod is disabled.</summary>
+        public bool InfiniteWater { get; set; } = true;
+
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>Set up Generic Mod Config Menu integration.</summary>
+        public static void SetUpModConfigMenu(ModConfig config, Mod mod)
+        {
+            // Get the Generic Mod Config Menu API
+            IGenericModConfigMenuApi api = mod.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (api is null) { return; }
+            var manifest = mod.ModManifest;
+
+            // Register the Generic Mod Config Menu API
+            api.Register(manifest, () => config = new ModConfig(), delegate { mod.Helper.WriteConfig(config); });
+
+            // Options to display
+            api.AddBoolOption(manifest, () => config.InfiniteWater, (bool val) => config.InfiniteWater = val,
+                    name: () => "Infinite Water", tooltip: () => null);
+            api.AddParagraph(manifest, text: () => "Infinite water in watering cans while true.");
+
+        }
+    }
+}
